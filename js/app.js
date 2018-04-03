6cbbc6939b47ec8de400dbdf068c8ffd15c6a7d0
BogdanPasterak <bogdanpasterak@gmail.com>
2018-04-03 19:42:38 +0100

    feat: First step up

--- js/app.js
+++ js/app.js
@@ -1,11 +1,22 @@
+// Variables used in the game
+const game = {
+    widthCanvas: 505,
+    levelPlayer: 0,
+    time: 0,
+    intervalID: undefined
+};
+
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x = 0, y = 83-20, speed = 50, sprite = 'images/enemy-bug.png' ) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
+    this.sprite = sprite;
 };
 
 // Update the enemy's position, required method for game
@@ -14,11 +25,31 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    // only enemy
+    if ( dt != undefined ) {
+        this.x += this.speed * dt;
+        // if outside the canvas
+        if (this.x > game.widthCanvas ) {
+            this.x = -100 * (Math.random() + 1);
+        }
+    }
 };
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+const Player = function(x = 202, y = 380) {
+    Enemy.call(this, x, y, 0);
+    this.sprite = 'images/char-boy.png'
+};
+Player.prototype = Object.create(Enemy.prototype);
+Player.prototype.constructor = Player;
+Player.prototype.update = function() {};
+Player.prototype.handleInput = function(way) {
+    if (way == 'up')
+        this.y -=20;
 };
 
 // Now write your own player class
@@ -29,6 +60,12 @@ Enemy.prototype.render = function() {
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+const allEnemies = new Array(
+    new Enemy(),
+    new Enemy(101, 83*2-20, 80)
+);
+
+const player = new Player();
 
 
 
@@ -44,3 +81,4 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
